71c47a042528fa1e6ff5be1a8c96a5256af96c33
Humaid Khan <humaidk2@gmail.com>
2017-08-16 04:07:44 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000; // 443
+
+const path = require('path');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+
+io.on('connection', function(socket) {
+  console.log('a user connected');
+
+  // io.to('some room').emit('some event');
+  socket.on('join', function(room) {
+    console.log('a user joined a room:', room);
+    socket.join(room);
+    // io.to(room).emit('test', 'room message from server sent');
+
+    socket.on('coordinates', function(coords) {
+      socket.broadcast.to(room).emit('otherPlayerCoords', coords);
+    });
+
+    socket.on('pelletCollision', function(pelletId) {
+      console.log('hit pellet collision in server', pelletId);
+      socket.broadcast.to(room).emit('otherPlayerPelletCollision', pelletId);
+    });
+
+    socket.on('leave', function(room) {
+      console.log('a user left the room:', room);
+      socket.leave(room);
+    })
+
+  });
+
+  socket.on('disconnect', function() {
+    console.log('user disconnected');
+  });
+
+});
+
+// MIDDLEWARE
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({'extended': true}));
+app.use(express.static(path.join(__dirname, '/client')));
+// app.use(flash());
+
+
+http.listen(port, function() {
+  console.log('Server is now connected on port ' + port);
+}).on('error', function(err) {
+  console.log('err:', err);
+});
