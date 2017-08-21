332ca54fe87f39a97e43fd691304f92ddd43a591
Humaid Khan <humaidk2@gmail.com>
2017-08-21 12:58:37 -0700

    install font-awesome

--- client/components/index.jsx
+++ client/components/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
 import App from './app';
+import "font-awesome-webpack";
 
 render(<App/>, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,8 @@ module.exports = {
   },
   module: {
     loaders: [
+       { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/ , loader: "url-loader?limit=10000&mimetype=application/font-woff" },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/ , loader: "file-loader" },
       {
         test: /\.jsx?/,
         exclude: /node_modules/,
@@ -18,7 +20,7 @@ module.exports = {
       {
         test: /\.json$/,
         loader: 'json-loader'
-      }
+      },
     ]
   },
   resolve: {
