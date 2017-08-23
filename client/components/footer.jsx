f29d0a55af9397f4be31384cce690a93318ceaa2
Humaid Khan <humaidk2@gmail.com>
2017-08-23 14:11:13 -0700

    Add font-awesome icons/links to footer Add portfolio, vrpacman, vrpacman-github links to footer

---
+++ client/components/footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer = props => (
+  <div className="footer">
+    <div className="icons">
+      <div className="icon"><a target="_blank" className="icon-link fa-stack fa-lg" href="https://angel.co/humaidk2"><i className="fa fa-stack-1x fa-angellist fa-inverse"></i></a></div>
+      <div className="icon"><a target="_blank" className="icon-link fa-stack fa-lg" href="https://github.com/humaidk2"><i className="fa fa-stack-1x fa-github fa-inverse"></i></a></div>
+      <div className="icon"><a target="_blank" className="icon-link fa-stack fa-lg" href="https://linkedin.com/in/humaidk2"><i className="fa fa-stack-1x fa-linkedin fa-inverse"></i></a></div>
+      <div className="icon"><a target="_blank" className="icon-link fa-stack fa-lg" href="https://twitter.com/humaidk2"><i className="fa fa-stack-1x fa-twitter fa-inverse"></i></a></div>
+      <div className="icon"><a className="icon-link fa-stack fa-lg" href="mailto:humaidk2@gmail.com"><i className="fa fa-stack-1x fa-envelope fa-inverse"></i></a></div>
+      <div className="icon"><a target="_blank" className="icon-link fa-stack fa-lg" style={{'display': 'flex'}} href="https://humaidkhan.com"><img src="assets/logo9.png" /></a></div>
+      <div className="icon"><a className="icon-link fa-stack fa-lg" href="https://vrpacman.com"><i className="fa fa-stack-1x fa-code fa-inverse"></i></a></div>
+      <div className="icon"><a className="icon-link fa-stack fa-lg" href="https://github.com/humaidk2/vrpacman"><i className="fa fa-stack-1x fa-code fa-inverse"></i></a></div>
+    </div>
+  </div>);
+
+export default Footer;
