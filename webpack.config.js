d54c69943c5f845a704320d927899fe798c4bd13
Humaid Khan <humaidk2@gmail.com>
2017-08-23 14:08:59 -0700

    prepare app for production

--- webpack.config.js
+++ webpack.config.js
@@ -29,10 +29,12 @@ module.exports = {
   devtool : '#source-map',
 
   plugins: [
-   new webpack.DefinePlugin({
-    'process.env': {
-      'NODE_ENV': '"development"'
-    }
-  })
-  ]
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': '"production"'
+      }
+    }),
+  new webpack.HotModuleReplacementPlugin(),
+  new webpack.optimize.UglifyJsPlugin()
+  ],
 };
